2fbef3693d1ee34de15029b32af99cb597c27f66
prints-philbert <119360081+prints-philbert@users.noreply.github.com>
2023-03-10 22:20:21 -0600

    working on script.js

--- script.js
+++ script.js
@@ -14,20 +14,21 @@ window.addEventListener("load", function() {
     form.addEventListener("submit", function(event) {
         event.preventDefault();
         console.log(pilotInput.value, copilotInput.value, fuelInput.value, cargoInput.value);
-        formSubmission(document, list, pilotInput, copilotInput, fuelInput, cargoInput);
+        formSubmission(document, list, pilotInput.value, copilotInput.value, fuelInput.value, cargoInput.value);
     });
 });
 
-//    let listedPlanets;
-//    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-//    let listedPlanetsResponse;
-//    listedPlanetsResponse.then(function (result) {
-//        listedPlanets = result;
-//        console.log(listedPlanets);
-//    }).then(function () {
-//        console.log(listedPlanets);
-//        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-//    })
+   let listedPlanets;
+   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   let listedPlanetsResponse = myFetch();
+   listedPlanetsResponse.then(function (result) {
+       listedPlanets = result;
+       console.log(listedPlanets);
+   }).then(function () {
+       console.log(listedPlanets);
+       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       
+   })
    
 
 
--- scriptHelper.js
+++ scriptHelper.js
@@ -3,17 +3,18 @@
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-   /*
+   let missionTarget = document.getElementById("missionTarget");
+   missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">
+    `
 }
 
 /* validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate. */
@@ -28,73 +29,47 @@ function validateInput(testInput) {
    }
 }
 
-console.log(`'A' is ${validateInput('A')}`);
-console.log(`1 ${validateInput(1)}`);
-console.log(`'' is ${validateInput('')}`);
-
-/* formSubmission() will take in a document parameter and strings
-representing the pilot, co-pilot, fuel level, and cargo mass. 
-Using the values in those strings and the document parameter for your HTML document, 
-update the shuttle requirements as described below. */
-
-/* The list of shuttle requirements, the div with the id faultyItems, should be updated if something is not ready for launch. 
-Using template literals, update the li elements pilotStatus and copilotStatus to include the pilot's name and the co-pilot's name.
-
-If the user submits a fuel level that is too low (less than 10,000 liters), change faultyItems to visible with an updated fuel status 
-stating that there is not enough fuel for the journey. The text of the h2 element, launchStatus, should also change to 
-"Shuttle not ready for launch" and the color should change to red.
-
-If the user submits a cargo mass that is too large (more than 10,000 kilograms), change the list to visible with an updated 
-cargo status stating that there is too much mass for the shuttle to take off. The text of launchStatus should also change to 
-"Shuttle not ready for launch" and the color should change to red.
-
-If the shuttle is ready to launch, change the text of launchStatus to green and display "Shuttle is ready for launch". */
-
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     
     let launchStatus = document.getElementById("launchStatus");
-    let faultyItems = document.getElementById("faultyItems");
     let pilotStatus = document.getElementById("pilotStatus");
     let copilotStatus = document.getElementById("copilotStatus");
     let fuelStatus = document.getElementById("fuelStatus");
     let cargoStatus = document.getElementById("cargoStatus");
 
-    launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-    launchStatus.color = 'red';
-    console.log(faultyItems);
-    list.visibility = 'visible';
-    console.log(list.visibility);
-    pilotStatus.innerHTML = `Pilot ${pilot.value}")} is ready for launch`;
-    console.log(pilot.value);
-    faultyItems.value += `
-        <div  id="faultyItems" data-testid="faultyItems">
-                <ol>
-                    <li id="pilotStatus" data-testid="pilotStatus">Pilot ${document.querySelector("input[name=pilotName]")} is ready for launch</li>
-                    <li id="copilotStatus" data-testid="copilotStatus">Co-pilot Ready</li>
-                    <li id="fuelStatus" data-testid="fuelStatus">Fuel level high enough for launch</li>
-                    <li id="cargoStatus" data-testid="cargoStatus">Cargo mass low enough for launch</li>
-                </ol>
-        </div>
-           
-    `;
-
-
-
-
+    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+        alert("Please include an input for each field.");
+    } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+        alert("Please enter a valid names and valid inputs for fuel level and cargo level.");
+    } else {
+            list.style.visibility = 'visible';
+            
+            pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+            copilotStatus.innerHTML = `Copilot ${copilot} is ready for launch`; 
+            
+            if ( fuelLevel < 10000 || cargoLevel > 10000 ) {
+                launchStatus.innerHTML = "Shuttle not ready for launch"
+                launchStatus.style.color = "red";
+            } else {
+                launchStatus.innerHTML = "Shuttle ready for launch"
+                launchStatus.style.color = "green";    
+            }
+    }
 }
 
-
-
 async function myFetch() {
-    let planetsReturned;
+    let planetsReturned = [];
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        response.json()
+    });
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+   let index = Math.floor(Math.random()*planets.length);
+   return planets[index];
 }
 
 // module.exports.addDestinationInfo = addDestinationInfo;
